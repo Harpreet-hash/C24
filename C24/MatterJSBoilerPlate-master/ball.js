598508a0f2aded7f3b64a88c739a381f41663e80
Harpreet-hash <73457305+Harpreet-hash@users.noreply.github.com>
2020-11-20 10:39:52 +0530

    Add files via upload
    crumpled ball 1

---
+++ C24/MatterJSBoilerPlate-master/ball.js
@@ -0,0 +1,33 @@
+class paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0.5,
+			density:1.2
+
+			
+			}
+		this.r=r;
+		this.body=Bodies.circle(x, y, r, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var paperpos=this.body.position;		
+
+			push()
+			translate(paperpos.x, paperpos.y);
+			rectMode(CENTER)
+			strokeWeight(3);
+			fill(255,0,255)
+			ellipse(0,0,2*this.r, 2*this.r);
+			pop()
+			
+	}
+
+}
